bb8b7fe5b0a478bad222974322078fe7119e612c
AdemolaAl <ademolaalameen86@gmail.com>
2025-07-18 09:05:06 +0100

    First commit

--- DialogBox.java
+++ DialogBox.java
@@ -2,7 +2,7 @@ import javax.swing.JOptionPane;
 public class DialogBox {
     public static void main(String[] args) {
         String result;
-        result = JOptionPane.showInputDialog(null,"What is your name?", "Name Test", JOptionPane.WARNING_MESSAGE);
+        result = JOptionPane.showInputDialog(null,"What is your name?", "Name Test", JOptionPane.QUESTION_MESSAGE);
         JOptionPane.showMessageDialog(null, "Your Name is " + result);
     }
 }
